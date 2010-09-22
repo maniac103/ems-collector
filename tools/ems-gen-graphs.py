c1d2726f3b7deca7882af60f1cbece122f82b366
Danny Baumann <dannybaumann@web.de>
2010-09-22 13:42:39 +0000

    Optimize gnuplot communication.

--- tools/ems-gen-graphs.py
+++ tools/ems-gen-graphs.py
@@ -27,7 +27,7 @@ def do_graphdata(sensor, filename):
         """ % (sensor, interval))
     datafile.close()
 
-def do_plot(name, filename, definitions):
+def do_plot(name, filename, ylabel, definitions):
     i = 1
     for definition in definitions:
         do_graphdata(definition[0], "/tmp/file%d.dat" % i)
@@ -42,44 +42,44 @@ def do_plot(name, filename, definitions):
     else:
         timeformat = "%d.%m"
 
-    gnuplotfile = open("/tmp/gnuplot.scr", "w")
-    gnuplotfile.write("""
-        set terminal png font 'arial' 12 size 1000,600
-        set grid lc rgb '#aaaaaa' lt 1 lw 0,5
-        set title '%s'
-        set xdata time
-        set xlabel "Datum"
-        set timefmt "%%Y-%%m-%%d %%H:%%M:%%S"
-        set format x "%s"
-        set xtic autofreq rotate by -45
-        set ytics autofreq
-        set output '%s'
-        \n
-        plot""" % (name, timeformat, os.path.join(targetpath, filename)))
+    process = subprocess.Popen("gnuplot", shell = False, stdin = subprocess.PIPE)
+    process.stdin.write("set terminal png font 'arial' 12 size 1000,600\n")
+    process.stdin.write("set grid lc rgb '#aaaaaa' lt 1 lw 0,5\n")
+    process.stdin.write("set title '%s'\n" % name)
+    process.stdin.write("set xdata time\n")
+    process.stdin.write("set xlabel 'Datum'\n")
+    process.stdin.write("set ylabel '%s'\n" % ylabel)
+    process.stdin.write("set timefmt '%Y-%m-%d %H:%M:%S'\n")
+    process.stdin.write("set format x '%s'\n" % timeformat)
+    process.stdin.write("set xtics autofreq rotate by -45\n")
+    process.stdin.write("set ytics autofreq\n")
+    process.stdin.write("set output '%s'\n" % os.path.join(targetpath, filename))
+    process.stdin.write("plot")
     for i in range(1, len(definitions) + 1):
         definition = definitions[i - 1]
-        gnuplotfile.write(" '/tmp/file%d.dat' using 1:3 with %s lw 2 title '%s'" %
-                          (i, definition[2], definition[1]))
+        process.stdin.write(" '/tmp/file%d.dat' using 1:3 with %s lw 2 title '%s'" %
+                           (i, definition[2], definition[1]))
         if i != len(definitions):
-            gnuplotfile.write(",")
-    gnuplotfile.close()
-    os.system("gnuplot /tmp/gnuplot.scr")
+            process.stdin.write(",")
+    process.stdin.write("\n")
+    process.stdin.close()
+    process.wait()
 
-definitions = [ [ 11, "Aussentemperatur", "lines smooth bezier" ],
-                [ 12, "Ged. Aussentemperatur", "lines" ] ]
-do_plot("Aussentemperatur", "aussentemp", definitions)
+definitions = [ [ 11, "Außentemperatur", "lines smooth bezier" ],
+                [ 12, "Ged. Außentemperatur", "lines" ] ]
+do_plot("Aussentemperatur", "aussentemp", "Temperatur (°C)", definitions)
 
 definitions = [ [ 13, "Raum-Soll", "lines" ],
                 [ 14, "Raum-Ist", "lines smooth bezier" ] ]
-do_plot("Raumtemperatur", "raumtemp", definitions)
+do_plot("Raumtemperatur", "raumtemp", "Temperatur (°C)", definitions)
 
 definitions = [ [ 1, "Kessel-Soll", "lines" ],
                 [ 2, "Kessel-Ist", "lines smooth bezier" ],
                 [ 6, "Vorlauf HK1", "lines smooth bezier" ],
                 [ 8, "Vorlauf HK2", "lines smooth bezier" ],
-                [ 10, "Ruecklauf", "lines smooth bezier" ] ]
-do_plot("Temperaturen", "kessel", definitions)
+                [ 10, "Rücklauf", "lines smooth bezier" ] ]
+do_plot("Temperaturen", "kessel", "Temperatur (°C)", definitions)
 
 definitions = [ [ 3, "Solltemperatur", "lines" ],
                 [ 4, "Isttemperatur", "lines smooth bezier" ] ]
-do_plot("Warmwasser", "ww", definitions)
+do_plot("Warmwasser", "ww", "Temperatur (°C)", definitions)
