6e3ae09efbb8a4d5421c9e4703bec5e849e62435
Michael Moosbauer <schule@michael-moosbauer.de>
2014-02-23 23:46:46 +0100

    Add RC subcommands minoutdoortemperature, buildingtype, outdoortempdamping, requestdata; add empty default handler to avoid ERRBUSY lockups, update help info

--- collector/CommandHandler.cpp
+++ collector/CommandHandler.cpp
@@ -196,7 +196,7 @@ CommandConnection::handleCommand(std::istream& request)
     request >> category;
 
     if (category == "help") {
-	respond("Available commands (help with '<command> help'):\nhk[1|2|3|4]\nuba\nrc\n");
+	respond("Available commands (help with '<command> help'):\nhk[1|2|3|4]\nww\nuba\nrc\ngetversion\n");
 	return Ok;
     } else if (category == "hk1") {
 	return handleHkCommand(request, 61);
@@ -228,10 +228,44 @@ CommandConnection::handleRcCommand(std::istream& request)
 
     if (cmd == "help") {
 	respond("Available subcommands:\n"
+                "minoutdoortemperature\n"
+                "buildingtype [light|medium|heavy]\n"
+                "outdoortempdamping\n"
+                "requestdata\n"
 		"geterrors\n"
 		"getcontactinfo\n"
 		"setcontactinfo [1|2] <text>\n");
 	return Ok;
+    } else if (cmd == "requestdata") {
+        startRequest(EmsProto::addressRC, 0xa5, 0, 25);
+        return Ok;
+    } else if (cmd == "minoutdoortemperature") {
+        return handleSingleByteValue(request, EmsProto::addressRC, 0xa5, 5, 1, -30, 10);
+    } else if (cmd == "buildingtype") {
+        std::string ns;
+        uint8_t data;
+
+        request >> ns;
+
+        if (ns == "light")         data = 0;
+        else if (ns == "medium")   data = 1;
+        else if (ns == "heavy")    data = 2;
+        else return InvalidArgs;
+
+        sendCommand(EmsProto::addressRC, 0xa5, 6, &data, 1);
+        return Ok;
+    } else if (cmd == "outdoortempdamping") {
+        uint8_t data;
+        std::string mode;
+
+        request >> mode;
+
+        if (mode == "on")       data = 0xff;
+        else if (mode == "off") data = 0x00;
+        else return InvalidArgs;
+
+        sendCommand(EmsProto::addressRC, 0xa5, 21, &data, 1);
+        return Ok;
     } else if (cmd == "getcontactinfo") {
 	startRequest(EmsProto::addressRC, 0xa4, 0, 42);
 	return Ok;
@@ -673,6 +707,7 @@ CommandConnection::handleWwCommand(std::istream& request)
 
     if (cmd == "help") {
 	respond("Available subcommands:\n"
+	        "mode [on|off|auto]\n"
 		"temperature <temp>\n"
 		"limittemperature <temp>\n"
 		"loadonce\n"
@@ -1081,6 +1116,12 @@ CommandConnection::handlePcMessage(const EmsMessage& message)
 		}
 	    }
 	    break;
+        case 0xa5: /* get system parameters */
+            done = true;  
+            break;
+        default:
+           /* unhandled message, do successfully nothing */
+           done = true;
 	}
     }
 
